d32b62d81480b0055b1096c1a9945925b5efd2bf
mambax7 <mambax7@gmail.com>
2020-08-05 04:01:59 -0400

    Fix: Flags don't show

--- xoops_version.php
+++ xoops_version.php
@@ -31,8 +31,8 @@ $moduleDirNameUpper = mb_strtoupper($moduleDirName);
 // ------------------- Informations ------------------- //
 $modversion = [
     'version'             => '3.21.0',
-    'module_status'       => 'RC 2',
-    'release_date'        => '2020/07/26',
+    'module_status'       => 'RC 3',
+    'release_date'        => '2020/08/04',
     'name'                => MI_XFGUESTBOOK_NAME,
     'description'         => MI_XFGUESTBOOK_DESC,
     'official'            => 0,
